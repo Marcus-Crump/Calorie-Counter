5fa34fbd8face7f588cad8b653c1615f29aea13a
Marcus L. Crump <m.crump@utexas.edu>
2025-11-17 10:44:25 -0600

    Init commit

---
+++ counter.py
@@ -0,0 +1,17 @@
+# An application to take a given list of main ingredients in a dish and give a caloric break down and a total
+
+# First, the user will create a TXT file **TODO Think about how/if it needs to be formatted and ways areounf this**
+
+# The program will then take the file **TODO Where is the file located, how will it find the file**
+
+# Then the program will tokenize the list of ingredients **TODO How will it do this with the formatting you will decide on**
+
+# The program will loop through the tokens and make websearches using the google search formatting 
+
+# Using BS4, the program will then pull the number from the search **TODO Are you sure all sample sizes are 100G**
+
+# It will store this info in a list **TODO how will this work for portions, are you going to make mom calaculate it**
+
+# Ideally, I want the program to take the measurements the user is putting in and figure out how many calories it has
+
+import requests
