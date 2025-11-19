fe5533dc612294779d33fc849bbb6edcc972ae05
Kaitlyn Martin <kat@Kaitlyns-Air.lan>
2025-11-19 12:37:05 -0600

    I should do a commit for each feature, tool, or function I implement so that people can better see the process

--- counter.py
+++ counter.py
@@ -1,15 +1,5 @@
 # An application to take a given list of main ingredients in a dish and give a caloric break down and a total
 
-# First, the user will create a TXT file **TODO Think about how/if it needs to be formatted and ways areounf this**
-
-# The program will then take the file **TODO Where is the file located, how will it find the file**
-
-# Then the program will tokenize the list of ingredients **TODO How will it do this with the formatting you will decide on**
-
-# The program will loop through the tokens and make websearches using the google search formatting 
-
-# Using BS4, the program will then pull the number from the search **TODO Are you sure all sample sizes are 100G**
-
 # It will store this info in a list **TODO how will this work for portions, are you going to make mom calaculate it**
 
 # Ideally, I want the program to take the measurements the user is putting in and figure out how many calories it has
@@ -18,9 +8,43 @@ import requests as req
 from bs4 import BeautifulSoup as bs
 import PyQt5 as gui
 
+def get_soup(url):
+    response = req.get(url)
+    soup = bs(response.text,'html.parser')
+    response.close()
+    return soup
+
+def get_food_url(food_split):
+    url = "https://www.myfooddata.com/search?search="
+    r = len(food_split)
+    for i in range(r):
+        url += food_split[i]
+        if i + 1 < r:
+            url+="%20"
+    return url
+
+def get_calories(soup):
+    return soup.find('td', class_="nft-cal-amt ENERC_KCAL").text.strip()
+
+def get_item_URL(food_split):
+    url = get_food_url(food_split)
+    soup = get_soup(url)
+    link = soup.find("a", class_="searchlink")
+    return link.text.strip()
+    
+
+
+def get_food_data(food):
+    item_URL = get_item_URL(food.split())
+    item_URL = item_URL[:len(item_URL)-3] + "100g/1"
+    soup = get_soup(item_URL)
+    kcal = get_calories(soup)
+    return kcal
+
 def main():
-    response = req.get("https://tools.myfooddata.com/nutrition-facts/171477/100g/1")
-    print(response.text)
+    foods = ["roast chicken","fusilli", "unsalted butter", "olive oil", "dried basil", "dried oregano"]
+    for item in foods:
+        print(get_food_data(item))
 
 
 if __name__ == "__main__":
